1faff1e7a4ca06493330049c8fb1befbf76220df
CarPoolCrypto <89317242+CarPoolCrypto@users.noreply.github.com>
2025-02-03 20:17:05 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,16 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 };
