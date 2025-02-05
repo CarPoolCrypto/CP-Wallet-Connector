668ade9a2bd946aeea8707f5713f4f078e77d173
CarPoolCrypto <89317242+CarPoolCrypto@users.noreply.github.com>
2025-02-04 19:45:49 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
-const path = require('path');
+const path = require("path")
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'build'),
+    filename: "index.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-};
+}
+
