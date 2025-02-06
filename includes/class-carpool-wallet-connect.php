21515d81b2d426a20036e7172f1300df9aa0a0ea
CarPoolCrypto <89317242+CarPoolCrypto@users.noreply.github.com>
2025-02-05 21:21:26 -0800

    Update class-carpool-wallet-connect.php

--- includes/class-carpool-wallet-connect.php
+++ includes/class-carpool-wallet-connect.php
@@ -1,5 +1,4 @@
 <?php
-// class-carpool-wallet-connect.php
 
 class CarPool_Wallet_Connect {
     private $api;
@@ -9,46 +8,29 @@ class CarPool_Wallet_Connect {
         $this->api = new CarPool_API();
         $this->user_roles = new CarPool_User_Roles();
 
-        add_action('wp_ajax_connect_wallet', array($this, 'connect_wallet'));
-        add_action('wp_ajax_nopriv_connect_wallet', array($this, 'connect_wallet'));
-        add_action('wp_ajax_update_delegation_info', array($this, 'update_delegation_info'));
+        add_action('wp_ajax_carpool_connect_wallet', array($this, 'connect_wallet'));
+        add_action('wp_ajax_nopriv_carpool_connect_wallet', array($this, 'connect_wallet'));
     }
 
     public function connect_wallet() {
         check_ajax_referer('carpool-wallet-connect', 'nonce');
-        
-        $wallet_address = sanitize_text_field($_POST['wallet_address']);
+
+        $address = sanitize_text_field($_POST['address']);
         $user_id = get_current_user_id();
 
-        if (!$user_id) {
-            wp_send_json_error(array('message' => 'User not logged in.'));
+        if ($user_id === 0) {
+            wp_send_json_error('User not logged in');
+            return;
         }
 
-        update_user_meta($user_id, 'carpool_wallet_address', $wallet_address);
+        update_user_meta($user_id, 'carpool_wallet_address', $address);
 
-        $this->update_delegation_info();
+        $delegation_info = $this->api->get_delegation_info($address);
+        update_user_meta($user_id, 'carpool_delegation_info', $delegation_info);
 
-        wp_send_json_success(array('message' => 'Wallet connected successfully'));
-    }
+        $this->user_roles->update_user_role($user_id, $delegation_info);
 
-    public function update_delegation_info() {
-        $user_id = get_current_user_id();
-
-        if (!$user_id) {
-            wp_send_json_error(array('message' => 'User not logged in.'));
-        }
-
-        $delegated_amount = $this->api->get_user_delegated_amount($user_id);
-        $delegation_epochs = $this->api->get_user_delegation_epochs($user_id);
-
-        update_user_meta($user_id, 'carpool_delegated_amount', $delegated_amount);
-        update_user_meta($user_id, 'carpool_delegation_epochs', $delegation_epochs);
-
-        $this->user_roles->update_user_role($user_id, $delegated_amount);
-
-        wp_send_json_success(array(
-            'delegated_amount' => $delegated_amount,
-            'delegation_epochs' => $delegation_epochs
-        ));
+        wp_send_json_success($delegation_info);
     }
 }
+
