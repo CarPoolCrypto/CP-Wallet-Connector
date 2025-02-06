48c4985a6da009fa460285931c81146d65289d10
CarPoolCrypto <89317242+CarPoolCrypto@users.noreply.github.com>
2025-02-05 21:54:52 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,15 @@ const path = require("path")
 module.exports = {
   entry: "./src/index.tsx",
   output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"],
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+    },
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
@@ -21,9 +25,17 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.wasm$/,
+        type: "webassembly/async",
       },
     ],
   },
+  experiments: {
+    asyncWebAssembly: true,
+  },
 }
+
 
