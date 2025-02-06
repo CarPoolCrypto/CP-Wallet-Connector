1e3d58784181f133eaeacb3dbfb4cb949774793f
CarPoolCrypto <89317242+CarPoolCrypto@users.noreply.github.com>
2025-02-05 21:20:55 -0800

    Update class-carpool-user-roles.php

--- includes/class-carpool-user-roles.php
+++ includes/class-carpool-user-roles.php
@@ -1,5 +1,4 @@
 <?php
-// class-carpool-user-roles.php
 
 class CarPool_User_Roles {
     public function __construct() {
@@ -8,39 +7,41 @@ class CarPool_User_Roles {
 
     public function register_user_roles() {
         add_role('carpool_delegator', 'CarPool Delegator', array('read' => true));
-        add_role('carpool_shrimp', 'CarPool Shrimp', array('read' => true));
-        add_role('carpool_tuna', 'CarPool Tuna', array('read' => true));
-        add_role('carpool_dolphin', 'CarPool Dolphin', array('read' => true));
-        add_role('carpool_shark', 'CarPool Shark', array('read' => true));
         add_role('carpool_whale', 'CarPool Whale', array('read' => true));
+        add_role('carpool_shark', 'CarPool Shark', array('read' => true));
+        add_role('carpool_dolphin', 'CarPool Dolphin', array('read' => true));
+        add_role('carpool_tuna', 'CarPool Tuna', array('read' => true));
+        add_role('carpool_shrimp', 'CarPool Shrimp', array('read' => true));
     }
 
-    public function update_user_role($user_id, $delegated_amount) {
+    public function update_user_role($user_id, $delegation_info) {
         $user = new WP_User($user_id);
         
         // Remove all CarPool roles
         $user->remove_role('carpool_delegator');
-        $user->remove_role('carpool_shrimp');
-        $user->remove_role('carpool_tuna');
-        $user->remove_role('carpool_dolphin');
-        $user->remove_role('carpool_shark');
         $user->remove_role('carpool_whale');
+        $user->remove_role('carpool_shark');
+        $user->remove_role('carpool_dolphin');
+        $user->remove_role('carpool_tuna');
+        $user->remove_role('carpool_shrimp');
 
-        // Assign new role based on delegated amount
-        if ($delegated_amount >= 1000000) {
-            $user->add_role('carpool_whale');
-        } elseif ($delegated_amount >= 100000) {
-            $user->add_role('carpool_shark');
-        } elseif ($delegated_amount >= 10000) {
-            $user->add_role('carpool_dolphin');
-        } elseif ($delegated_amount >= 1000) {
-            $user->add_role('carpool_tuna');
-        } elseif ($delegated_amount > 0) {
-            $user->add_role('carpool_shrimp');
-        }
-
-        if ($delegated_amount > 0) {
+        if ($delegation_info['is_delegated']) {
             $user->add_role('carpool_delegator');
+
+            // Assign role based on delegated amount
+            $amount = $delegation_info['amount'];
+            if ($amount >= 500000) {
+                $user->add_role('carpool_whale');
+            } elseif ($amount >= 150000) {
+                $user->add_role('carpool_shark');
+            } elseif ($amount >= 50000) {
+                $user->add_role('carpool_dolphin');
+            } elseif ($amount >= 10000) {
+                $user->add_role('carpool_tuna');
+            } else {
+                $user->add_role('carpool_shrimp');
+            }
         }
     }
 }
+
